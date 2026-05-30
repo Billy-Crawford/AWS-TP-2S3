ca8daf77b51f097beba53ed43284e8b53a599b5d
Billy-Craweford <oumaroubilly6@gmail.com>
2026-05-30 15:08:15 +0200

    j'ai retirer mon code du template et j'utilise le main.py

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import json
+
+def lambda_handler(event, context):
+    print("Event:", json.dumps(event))
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Hello from Lambda! from src/main.py")
+    }
+
+    
