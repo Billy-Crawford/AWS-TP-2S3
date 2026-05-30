7447fa1b6c5af27bf1648bba0f26b0d6e4c89451
Billy-Craweford <oumaroubilly6@gmail.com>
2026-05-30 17:14:54 +0200

    verification de l'existance si non on ajoute - on utilise aussi les variables d'env

--- src/main.py
+++ src/main.py
@@ -1,11 +1,55 @@
 import json
+import boto3
+import os
+
+TABLE_NAME = os.environ["TABLE_NAME"]
+PK_VALUE = os.environ["PK_VALUE"]
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(TABLE_NAME)
+
 
 def lambda_handler(event, context):
-    print("Event:", json.dumps(event))
+
+    print("EVENT:", json.dumps(event))
+
+    record = event["Records"][0]
+
+    bucket = record["s3"]["bucket"]["name"]
+    key = record["s3"]["object"]["key"]
+
+    print(f"Bucket={bucket}")
+    print(f"Key={key}")
+
+    # Vérifier si déjà enregistré
+    response = table.get_item(
+        Key={
+            "PK": PK_VALUE,
+            "SK": key
+        }
+    )
+
+    if "Item" in response:
+        print("Fichier déjà traité")
+        return {
+            "statusCode": 200,
+            "body": json.dumps("Already processed")
+        }
+
+    # Sinon enregistrer
+    table.put_item(
+        Item={
+            "PK": PK_VALUE,
+            "SK": key,
+            "bucket": bucket
+        }
+    )
+
+    print("Fichier enregistré")
 
     return {
         "statusCode": 200,
-        "body": json.dumps("Hello from Lambda! from src/main.py")
+        "body": json.dumps("Processed")
     }
 
     
